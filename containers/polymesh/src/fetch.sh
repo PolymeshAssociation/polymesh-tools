15b7a20b222435fd16b9f07d32b2176bc1ce31f2
Ayrton Freeman <ayrton@riseup.net>
2022-09-05 12:39:18 -0300

    Update app version to 5.0.2 and fix depreciations (#24)
    * Update app version to 5.0.0
    
    * Add new docker repository reference
    
    * Add new telemetry default url
    
    * Fix imagePullError from new repository
    
    * Fix fetch script
    
    * Fix entrypoint for 5.0.2 distroless
    
    * Remove telemetry completely to use runtime defaults

--- containers/polymesh/src/fetch.sh
+++ containers/polymesh/src/fetch.sh
@@ -5,23 +5,17 @@ set -x
 set -o pipefail
 
 apt-get --quiet update
-apt-get --quiet --assume-yes install curl wget
-curl -s https://api.github.com/repos/polymathnetwork/polymesh/releases/latest | \
-  grep "browser_download_url.*linux-amd64" | \
+apt-get --quiet --assume-yes install curl wget unzip
+curl -s https://api.github.com/repos/polymeshassociation/polymesh/releases/latest | \
+  grep "browser_download_url" | grep -v "wasm" | \
   awk '{print $2}' | \
   xargs wget --quiet
-sha256sum -c *.sha256
-tar xvzf *.tgz
+unzip *.zip
+rm *.zip
+mv polymesh-* polymesh
 mkdir -p /opt/files/usr/local/bin
 mkdir -p /opt/files/var/lib/polymesh
 mkdir -p /opt/files/lib/x86_64-linux-gnu
 
-mv $(find . -type f -executable -print) /opt/files/usr/local/bin/polymesh 
-touch /opt/files/var/lib/polymesh/.keep
-cp -a /lib/x86_64-linux-gnu/* /opt/files/lib/x86_64-linux-gnu/
-LDLIBS=$(ldd /opt/files/usr/local/bin/polymesh | grep -o '/\S*')
-for LIB in $LDLIBS; do
-    mkdir -p /opt/files/$(dirname $LIB | cut -c 2-)
-    cp $LIB  /opt/files/$(dirname $LIB | cut -c 2-)/
-done
+mv polymesh /opt/files/usr/local/bin/
 
